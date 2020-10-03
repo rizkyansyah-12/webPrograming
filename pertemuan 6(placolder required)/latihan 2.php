5a586a2ab8f06913dacdfd0577cfb6e4220a5f43
rizkyansyah-12 <rizkyansyah920@gmail.com>
2020-10-03 11:04:59 +0700

    input dalam data base

--- pertemuan 6(placolder required)/latihan 2.php
+++ pertemuan 6(placolder required)/latihan 2.php
@@ -5,41 +5,37 @@ $password = '';
 $database = 'database-php';
 
 $koneksi = mysqli_connect($localhost, $username, $password, $database);
+if (! $koneksi){
+    die("koneksi gagal: ". mysqli_connect_error());
+}
+echo "koneksi berhasil";
 
 if (isset($_POST["submit"])) {
     $nama = $_POST['nama'];
     $tempat_lahir = $_POST['tempat_lahir'];
-    $tanggal_lahir = $_POST['tanggal_lahir'];
-    $nik = $_POST['nik'];
+    $tgl_lahir = $_POST['tgl_lahir'];
+    $nik_nip = $_POST['nik_nip'];
     $email = $_POST['email'];
     $telp = $_POST['telp'];
     $telp_wali = $_POST['telp_wali'];
     $pekerjaan = $_POST['pekerjaan'];
     $alamat = $_POST['alamat'];
-    $prog_akademik = $_POST['prog_akademik'];
+    $prog_akademi = $_POST['prog_akademi'];
     $tema_pelatihan = $_POST['tema_pelatihan'];
-    $tempat_pelatihan = $_POST['tempat_pelatihan'];
     $mitra_pelatihan = $_POST['mitra_pelatihan'];
 
-    echo "Data Masuk!";
-    $result = "INSERT INTO registrasi VALUES ( '' , '$nama' , '$tempat_lahir', '$tanggal_lahir','$nik','$email','$telp','$telp_wali','$pekerjaan','$alamat','$prog_akademik','$tema_pelatihan', '$tempat_pelatihan','$mitra_pelatihan' )";
-    $sql = mysqli_master_query($koneksi, $result);
+    //inser data
+    $result = "INSERT INTO registrasi VALUES ('', '$nama','$tempat_lahir','$tgl_lahir', '$nik_nip', '$email', 'telp', $'telp_wali', 'pekerjaan', '$alamat', '$prog_akademi', '$tema_pelatihan', '$mitra_pelatihan')";
+    $sql = mysqli_query($koneksi, $result);
 
+    //pencegahan data berhasil masuk dan/gagal
     if ($sql) {
         echo " <script> alert ('Data Berhasil Ditambahkan');</script> ";
     }   else{
         echo " <script> alert ('Data Gagal Ditambahkan');</script> ";
     }
- }
-
-    
-    ?>
-
-
-
-
-
-
+}
+?>
 
 <!DOCTYPE html>
 <Html lang="en">
@@ -52,86 +48,83 @@ if (isset($_POST["submit"])) {
     <tr>
         <td>
             <table border="1">
-                  <tr>
-                      <td align="center" width="800"><b>FORM KOMITMEN PARTISIPASI<br>PROGRAM PEMBERIAN BANTUAN PEMERINAH<br>DIGITAL TALENT SCHOOLARSHIP TAHUN 2020</b></td>
-                  </tr>
-            </table>
-                <table>
-                  <tr>
-                      <td>
-            <table border="1">
+              <tr>
+                  <td align="center" width="800"><b>FORM KOMITMEN PARTISIPASI<br>PROGRAM PEMBERIAN BANTUAN PEMERINAH<br>DIGITAL TALENT SCHOOLARSHIP TAHUN 2020</b></td>
+              </tr>
+          </table>
+          <table>
+              <tr>
+                  <td>
+                    <table border="1">
                         <tr>
                             <td> <i> Formulir ini untuk diisi (di ketik atau di tulis tangan dengan jelas) kemudian di unggah/upload di akun digitalent.kominfo.go.id oleh masing-masing peserta di <b> awal</b> pelaksanaan pelatihan.</i></td>
-                  </tr>
-            </table>
-                <form action="" method="POST">
-                <table border="1">
-                        <tr>
-                            <td>Saya Yang bertandatangan di bawah ini</td>
-                            <td>:</td>
+                        </tr>
+                    </table>
+                    <form action="" method="POST">
+                        <table border="1">
+                            <tr>
+                                <td>Saya Yang bertandatangan di bawah ini</td>
+                                <td>:</td>
+                            </tr>
+                            <tr>
+                                <td><label for="nama">Nama Lengkap (Sesuai KTP)</label> </td>
+                                <td> : <input type="text" name="nama" placeholder="Masukan Nama Lengkap" required></td>
+                            </tr>
+                            <tr>
+                                <td><label for="tempat_lahir">Tempat Lahir </label> </td>
+                                <td> : <input type="text" name="tempat_lahir" placeholder="Masukan tempat_Lahir" required><br></td>
+                            </tr>
+                            <tr>
+                             <td><label for="tgl_lahir">Tanggal Lahir </label> </td>
+                             <td>: <input type="date" name="tgl_lahir" required><br></td>
+                         </tr>
+                         <tr>
+                            <td><label for="nik_nip">NIK_NIP </label> </td>
+                            <td>: <input type="text" name="nik_nip" placeholder="Masukan NIKN_IP" required></td>
                         </tr>
                         <tr>
-                            <td>Nama Lengkap (Sesuai KTP) </td>
-                            <td> : <input type="text" name="nama" placeholder="Masukan Nama Lengkap" required></td>
+                            <td><label for="email">Email </label> </td>
+                            <td>: <input type="text" name="email" placeholder="Masukan email" required></td>
                         </tr>
                         <tr>
-                            <td>Tempat Lahir </td>
-                            <td> : <input type="text" name="tempat_lahir" placeholder="Masukan Tempat Lahir" required><br></td>
-                       </tr>
-                       <tr>
-                           <td>Tanggal Lahir </td>
-                           <td>: <input type="date" name="tanggal_lahir" required><br></td>
-                       </tr>
-                       <tr>
-                            <td>NIK/NIP</td>
-                            <td>: <input type="text" name="NIK/NIP" placeholder="Masukan NIK/NIP" required></td>
+                            <td><label for="telp">Telepon </label> </td>
+                            <td>: <input type="text" name="telp" placeholder="Masukan telp" required></td>
                         </tr>
                         <tr>
-                            <td>E-Mail Aktif</td>
-                            <td>: <input type="text" name="email" placeholder="Masukan Email" required></td>
+                            <td><label for="telp_wali">Telepon Wali </label> </td>
+                            <td>: <input type="text" name="telp_wali" placeholder="Masukan telp_wali"required></td>
                         </tr>
                         <tr>
-                            <td>No. Hp Aktif </td>
-                            <td>: <input type="text" name="telp" placeholder="Masukan No Telp" required></td>
-                        </tr>
-                        <tr>
-                            <td>No Hp (Wali/Atasan) </td>
-                            <td>: <input type="text" name="telp_wali" placeholder="Masukan No Telp"required></td>
-                        </tr>
-                        <tr>
-                            <td>Pekerjaan</td>
+                            <td><label for="Pekerjaan"> Pekerjaan </label></td>
                             <td>: <input type="text" name="pekerjaan" placeholder="Pekerjaan" required></td>
                         </tr>
                         <tr>
-                            <td>Alamat</td>
+                           <td><label for="Alamat"> Alamat </label></td>
                             <td>: <textarea id="alamat" cols="40" rows="5" name="alamat" placeholder="Masukan Alamat" required></textarea><br></td>
                         </tr>
                         <tr>
-                            <td>Program Akademik</td>
-                            <td>: <input type="text" name="prog_akademik" placeholder="Masukan Program Akademik" required></td>
+                            <td><label for="prog_akademi"> Program Akademi </label></td>
+                            <td>: <input type="text" name="prog_akademi" placeholder="Masukan program_akademi" required></td>
                         </tr>
                         <tr>
-                            <td>Tema Pelatihan</td>
-                            <td>: <input type="text" name="tema_pelatihan" placeholder="Masukan Tema Pelatihan" required></td>
+                           <td><label for="tema_pelatihan"> Tema Pelatihan </label></td>
+                            <td>: <input type="text" name="tema_pelatihan" placeholder="Masukan tema_pelatihan" required></td>
                         </tr>
-                        <tr>
-                            <td>Tempat Pelatihan</td>
-                            <td>: <input type="text" name="tempat_pelatihan" placeholder="Masukan Tempat Pelatihan" required></td>
-                        </tr>
-                        <tr>
-                            <td>Mitra Pelatihan</td>
-                            <td>: <input type="text" name="mitra_pelatihan" placeholder="Masukan Mitra Pelatihan" required></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><button type="submit" type="submit">Kirim</button><button type="submit" type="submit">Batal</button></td>
-                        </tr>
-               </table>
-                </form>
-                    <table>
-                      <tr>
-                          <td>
-                            <table  border="1" cellpadding="2" cellspacing="0">
+                    </tr>
+                    <tr>
+                        <td><label for="mitra_pelatihan"> Mitra Pelatihan </label></td>
+                        <td>: <input type="text" name="mitra_pelatihan" placeholder="Masukan mitra_pelatihan" required></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button type="submit" name="submit">Kirim</button>| <button type="button" type="submit">Batal</button></td>
+                    </tr>
+                </table>
+            </form>
+            <table>
+              <tr>
+                  <td>
+                    <table  border="1" cellpadding="2" cellspacing="0">
                         <tr>
                             <td>No</td>
                             <td>Nama</td>
@@ -159,13 +152,13 @@ if (isset($_POST["submit"])) {
                             <td>agus@gmail.com</td>
                             <td><button>Edit</button> <button>Hapus</button></td>
                         </tr>
-                       
+
                     </table>
-                    </td>
-                  </tr>
-              </table>
-        </td>
-    </tr>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
 </table>
 
 </body>
